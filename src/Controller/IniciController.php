e42205efb1f94bae7346b1e513c71d658c299f97
ismaelBouassami <ismbou9@alu.edu.gva.es>
2024-01-04 21:58:21 +0100

    Practica 3 a medias

--- src/Controller/IniciController.php
+++ src/Controller/IniciController.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace App\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,7 +10,7 @@ class IniciController extends AbstractController
     #[Route('/', name: 'inici')]
     public function inici(): Response
     {
-        return new Response ('Gestión de equipos del proyecto de 2n de DAW');
-        
+       return $this->render('inici/inici.html.twig');
+
     }
 }
